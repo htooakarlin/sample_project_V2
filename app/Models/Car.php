d63502353040d9bcae8fc23b4bdcdc4e307b0e4c
Zin Zin Zaw <zinzinzaw04d@gmail.com>
2024-12-11 14:32:56 +0630

    Database for car_sales

---
+++ app/Models/Car.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Car extends Model
+{
+    use HasFactory;
+
+    public function customer() {
+        return $this->belongsTo(Customer::class);
+    }
+    
+    public function purchase() {
+        return $this->hasOne(Purchase::class);
+    }
+    
+}
